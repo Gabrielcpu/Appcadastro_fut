e59d22ae6527a54c729c0980049605f9f5174302
gabrielmatos-boop <gabriel.matos@cliconsultoria.com.br>
2026-06-10 17:39:59 -0300

    Adicionar arquivos de projeto.

---
+++ Appcadastro_fut/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Appcadastro_fut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtJogador = new System.Windows.Forms.TextBox();
+            this.txtSelecao = new System.Windows.Forms.TextBox();
+            this.chkPossui = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.dgvFigurinhas = new System.Windows.Forms.DataGridView();
+            this.colCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colSelecao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colJogador = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colPossui = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.picFoto = new System.Windows.Forms.PictureBox();
+            this.btnSelecionarFoto = new System.Windows.Forms.Button();
+            this.cmbTipo = new System.Windows.Forms.ComboBox();
+            this.chkDesejado = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFigurinhas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(44, 37);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 22);
+            this.txtCodigo.TabIndex = 0;
+            // 
+            // txtJogador
+            // 
+            this.txtJogador.Location = new System.Drawing.Point(44, 152);
+            this.txtJogador.Name = "txtJogador";
+            this.txtJogador.Size = new System.Drawing.Size(100, 22);
+            this.txtJogador.TabIndex = 1;
+            // 
+            // txtSelecao
+            // 
+            this.txtSelecao.Location = new System.Drawing.Point(44, 93);
+            this.txtSelecao.Name = "txtSelecao";
+            this.txtSelecao.Size = new System.Drawing.Size(100, 22);
+            this.txtSelecao.TabIndex = 2;
+            // 
+            // chkPossui
+            // 
+            this.chkPossui.AutoSize = true;
+            this.chkPossui.Location = new System.Drawing.Point(44, 206);
+            this.chkPossui.Name = "chkPossui";
+            this.chkPossui.Size = new System.Drawing.Size(139, 20);
+            this.chkPossui.TabIndex = 3;
+            this.chkPossui.Text = "Possuo a figurinha";
+            this.chkPossui.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "codigo da figurinha";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "selecao";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "jogador";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(303, 24);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 7;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click_1);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(303, 53);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 8;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(303, 93);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 9;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click_1);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(303, 125);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 10;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click_1);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(303, 155);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 11;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // dgvFigurinhas
+            // 
+            this.dgvFigurinhas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFigurinhas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colCodigo,
+            this.colSelecao,
+            this.colJogador,
+            this.colPossui});
+            this.dgvFigurinhas.Location = new System.Drawing.Point(486, 15);
+            this.dgvFigurinhas.Name = "dgvFigurinhas";
+            this.dgvFigurinhas.RowHeadersWidth = 51;
+            this.dgvFigurinhas.RowTemplate.Height = 24;
+            this.dgvFigurinhas.Size = new System.Drawing.Size(578, 159);
+            this.dgvFigurinhas.TabIndex = 12;
+            this.dgvFigurinhas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFigurinhas_CellClick);
+            // 
+            // colCodigo
+            // 
+            this.colCodigo.HeaderText = "Código";
+            this.colCodigo.MinimumWidth = 6;
+            this.colCodigo.Name = "colCodigo";
+            this.colCodigo.Width = 125;
+            // 
+            // colSelecao
+            // 
+            this.colSelecao.HeaderText = "Seleção";
+            this.colSelecao.MinimumWidth = 6;
+            this.colSelecao.Name = "colSelecao";
+            this.colSelecao.Width = 125;
+            // 
+            // colJogador
+            // 
+            this.colJogador.HeaderText = "Jogador";
+            this.colJogador.MinimumWidth = 6;
+            this.colJogador.Name = "colJogador";
+            this.colJogador.Width = 125;
+            // 
+            // colPossui
+            // 
+            this.colPossui.HeaderText = "Possui";
+            this.colPossui.MinimumWidth = 6;
+            this.colPossui.Name = "colPossui";
+            this.colPossui.Width = 125;
+            // 
+            // picFoto
+            // 
+            this.picFoto.Location = new System.Drawing.Point(486, 206);
+            this.picFoto.Name = "picFoto";
+            this.picFoto.Size = new System.Drawing.Size(578, 212);
+            this.picFoto.TabIndex = 13;
+            this.picFoto.TabStop = false;
+            // 
+            // btnSelecionarFoto
+            // 
+            this.btnSelecionarFoto.Location = new System.Drawing.Point(303, 351);
+            this.btnSelecionarFoto.Name = "btnSelecionarFoto";
+            this.btnSelecionarFoto.Size = new System.Drawing.Size(143, 35);
+            this.btnSelecionarFoto.TabIndex = 14;
+            this.btnSelecionarFoto.Text = "Selecionar Foto";
+            this.btnSelecionarFoto.UseVisualStyleBackColor = true;
+            this.btnSelecionarFoto.Click += new System.EventHandler(this.btnSelecionarFoto_Click);
+            // 
+            // cmbTipo
+            // 
+            this.cmbTipo.FormattingEnabled = true;
+            this.cmbTipo.Items.AddRange(new object[] {
+            "Comum",
+            "Especial"});
+            this.cmbTipo.Location = new System.Drawing.Point(303, 184);
+            this.cmbTipo.Name = "cmbTipo";
+            this.cmbTipo.Size = new System.Drawing.Size(121, 24);
+            this.cmbTipo.TabIndex = 15;
+            // 
+            // chkDesejado
+            // 
+            this.chkDesejado.AutoSize = true;
+            this.chkDesejado.Location = new System.Drawing.Point(44, 245);
+            this.chkDesejado.Name = "chkDesejado";
+            this.chkDesejado.Size = new System.Drawing.Size(89, 20);
+            this.chkDesejado.TabIndex = 16;
+            this.chkDesejado.Text = "Desejado";
+            this.chkDesejado.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 450);
+            this.Controls.Add(this.chkDesejado);
+            this.Controls.Add(this.cmbTipo);
+            this.Controls.Add(this.btnSelecionarFoto);
+            this.Controls.Add(this.picFoto);
+            this.Controls.Add(this.dgvFigurinhas);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkPossui);
+            this.Controls.Add(this.txtSelecao);
+            this.Controls.Add(this.txtJogador);
+            this.Controls.Add(this.txtCodigo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFigurinhas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtJogador;
+        private System.Windows.Forms.TextBox txtSelecao;
+        private System.Windows.Forms.CheckBox chkPossui;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.DataGridView dgvFigurinhas;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colSelecao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colJogador;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colPossui;
+        private System.Windows.Forms.PictureBox picFoto;
+        private System.Windows.Forms.Button btnSelecionarFoto;
+        private System.Windows.Forms.ComboBox cmbTipo;
+        private System.Windows.Forms.CheckBox chkDesejado;
+    }
+}
+
---
+++ Appcadastro_fut/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Appcadastro_fut
+{
+    public partial class Form1 : Form
+    {
+        private string arquivo = "figurinhas.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbTipo.Items.Add("Comum");
+            cmbTipo.Items.Add("Especial");
+
+            CarregarGrid();
+        }
+
+        private void CarregarGrid()
+        {
+            dgvFigurinhas.Rows.Clear();
+
+            if (!File.Exists(arquivo))
+                return;
+
+            string[] linhas = File.ReadAllLines(arquivo);
+
+            foreach (string linha in linhas)
+            {
+                if (string.IsNullOrWhiteSpace(linha))
+                    continue;
+
+                string[] dados = linha.Split(';');
+
+                if (dados.Length < 4)
+                    continue;
+
+                dgvFigurinhas.Rows.Add(
+                    dados[0],
+                    dados[1],
+                    dados[2],
+                    dados[3]
+                );
+            }
+        }
+
+        private void LimparCampos()
+        {
+         
+            txtCodigo.Clear();
+            txtSelecao.Clear();
+            txtJogador.Clear();
+
+            cmbTipo.SelectedIndex = -1;
+
+            chkPossui.Checked = false;
+            chkDesejado.Checked = false;
+
+            caminhoImagem = "";
+            picFoto.Image = null;
+
+            txtCodigo.Focus();
+        }
+        
+ 
+
+        private void dgvFigurinhas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0)
+                return;
+
+            txtCodigo.Text =
+                dgvFigurinhas.Rows[e.RowIndex].Cells[0].Value.ToString();
+
+            txtSelecao.Text =
+                dgvFigurinhas.Rows[e.RowIndex].Cells[1].Value.ToString();
+
+            txtJogador.Text =
+                dgvFigurinhas.Rows[e.RowIndex].Cells[2].Value.ToString();
+
+            string[] linhas = File.ReadAllLines(arquivo);
+
+            foreach (string linha in linhas)
+            {
+                string[] dados = linha.Split(';');
+
+                if (dados[0] == txtCodigo.Text)
+                {
+                    cmbTipo.Text = dados[3];
+
+                    chkPossui.Checked =
+                        Convert.ToBoolean(dados[4]);
+
+                    chkDesejado.Checked =
+                        Convert.ToBoolean(dados[5]);
+
+                    caminhoImagem = dados[6];
+
+                    if (File.Exists(caminhoImagem))
+                        picFoto.ImageLocation = caminhoImagem;
+
+                    break;
+                }
+            }
+        }
+
+        private void btnCadastrar_Click_1(object sender, EventArgs e)
+        {
+            if (
+        txtCodigo.Text.Trim() == "" ||
+        txtSelecao.Text.Trim() == "" ||
+        txtJogador.Text.Trim() == "" ||
+        cmbTipo.Text == "" ||
+        caminhoImagem == ""
+       )
+            {
+                MessageBox.Show("Preencha todos os campos obrigatórios.");
+                return;
+            }
+
+            string linha =
+                txtCodigo.Text.Trim() + ";" +
+                txtSelecao.Text.Trim() + ";" +
+                txtJogador.Text.Trim() + ";" +
+                cmbTipo.Text + ";" +
+                chkPossui.Checked + ";" +
+                chkDesejado.Checked + ";" +
+                caminhoImagem;
+
+            File.AppendAllText(
+                arquivo,
+                linha + Environment.NewLine);
+
+            MessageBox.Show("Figurinha cadastrada com sucesso!");
+
+            LimparCampos();
+            CarregarGrid();
+        }
+
+        private void btnAlterar_Click_1(object sender, EventArgs e)
+        {
+            if (!File.Exists(arquivo))
+            {
+                MessageBox.Show("Arquivo não encontrado.");
+                return;
+            }
+
+            List<string> linhas =
+                File.ReadAllLines(arquivo).ToList();
+
+            bool encontrou = false;
+
+            for (int i = 0; i < linhas.Count; i++)
+            {
+                string[] dados = linhas[i].Split(';');
+
+                if (dados[0] == txtCodigo.Text.Trim())
+                {
+                    linhas[i] =
+                     txtCodigo.Text.Trim() + ";" +
+                     txtSelecao.Text.Trim() + ";" +
+                     txtJogador.Text.Trim() + ";" +
+                     cmbTipo.Text + ";" +
+                     chkPossui.Checked + ";" +
+                     chkDesejado.Checked + ";" +
+                     caminhoImagem;
+
+                    encontrou = true;
+                    break;
+                }
+            }
+
+            if (!encontrou)
+            {
+                MessageBox.Show("Figurinha não encontrada.");
+                return;
+            }
+
+            File.WriteAllLines(arquivo, linhas);
+
+            MessageBox.Show("Figurinha alterada com sucesso!");
+
+            LimparCampos();
+            CarregarGrid();
+        }
+
+        private void btnExcluir_Click_1(object sender, EventArgs e)
+        {
+            if (!File.Exists(arquivo))
+            {
+                MessageBox.Show("Arquivo não encontrado.");
+                return;
+            }
+
+            List<string> linhas =
+                File.ReadAllLines(arquivo).ToList();
+
+            linhas.RemoveAll(x =>
+                x.Split(';')[0] == txtCodigo.Text.Trim());
+
+            File.WriteAllLines(arquivo, linhas);
+
+            MessageBox.Show("Figurinha excluída com sucesso!");
+
+            LimparCampos();
+            CarregarGrid();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void btnSelecionarFoto_Click(object sender, EventArgs e)
+        {
+        
+            OpenFileDialog abrir = new OpenFileDialog();
+
+            abrir.Filter = "Imagens|*.jpg;*.jpeg;*.png";
+
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                string pastaFotos = "Fotos";
+
+                if (!Directory.Exists(pastaFotos))
+                    Directory.CreateDirectory(pastaFotos);
+
+                string nomeArquivo = Path.GetFileName(abrir.FileName);
+
+                string destino = Path.Combine(pastaFotos, nomeArquivo);
+
+                File.Copy(abrir.FileName, destino, true);
+
+                caminhoImagem = destino;
+
+                picFoto.ImageLocation = caminhoImagem;
+            }
+        }
+        
+        
+        private string caminhoImagem = "";
+
+        
+    }
+}
